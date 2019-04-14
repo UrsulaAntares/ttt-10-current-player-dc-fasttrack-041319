ebc6edfce0f3624ce088a57eabc402991248820b
Ursula Miller <ursula.miller+fi@gmail.com>
2019-04-14 16:36:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,14 @@
-def current_player()
-  
+def turn_count(board)
+  count = 0
+  board.each do |cell|
+    if cell == "X" || cell == "O"
+      count += 1 
+  end
 end
   
-def current_player(board)
   
+  
+  
+def current_player(board)
+  turns_taken = turn_count(board)
 end
